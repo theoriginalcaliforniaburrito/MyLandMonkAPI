ce4c21a703ead62a2190dac693df924c0e8e6c8a
theoriginalcaliforniaburrito <47760827+theoriginalcaliforniaburrito@users.noreply.github.com>
2019-04-23 17:51:29 -0700

    Generic Repository
    V 1.0

---
+++ Repository/UnitRepository.cs
@@ -0,0 +1,55 @@
+using System;
+// using Contracts;
+using Entities;
+using Entities.Extensions;
+using Entities.ExtendedModels;
+using Entities.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Repository
+{
+    class UnitRepository : RepositoryBase<Unit>, IUnitRepository
+    {
+        public UnitRepository(RepositoryContext repositoryContext)
+            :base(repositoryContext)
+        {
+            
+        }
+
+        public IEnumerable<Unit> UnitsByProperty(Guid propertyId)
+        {
+            return FindByCondition(a => a.PropertyId.Equals(propertyId));
+        }
+
+        public IEnumerable<Unit> GetAllUnits()
+        {
+            return FindAll()
+                .OrderBy(o => o.DateCreated);
+        }
+        public Unit GetUnitById(Guid unitId)
+        {
+            return FindByCondition(unit => unit.Id.Equals(unitId))
+            .DefaultIfEmpty(new Unit())
+            .FirstOrDefault();
+        }
+        public void CreateUnit(Unit unit)
+        {
+            unit.Id = Guid.NewGuid();
+            Create(unit);
+            Save();
+        }
+        public void UpdateUnit(Unit dbUnit, Unit unit)
+        {
+            dbUnit.Map(unit);
+            Update(dbUnit);
+            Save();
+        }
+
+        public void DeleteUnit(Unit unit)
+        {
+            Delete(unit);
+            Save();
+        }
+    }
+}
