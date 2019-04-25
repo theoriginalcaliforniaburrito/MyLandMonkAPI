034f4a5ee0d56f2eeabf02a550bc0673019904f7
Michaelxk-CodingSamurai <49213568+Michaelxk-CodingSamurai@users.noreply.github.com>
2019-04-24 17:05:44 -0700

    Minor Changes. Waiting for Repository

---
+++ LandMonkAPI/LandMonkServer/Controllers/TenantController.cs
@@ -0,0 +1,69 @@
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System; 
+
+
+namespace TenantServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TenantController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        private IRepositoryWrapper _repository;
+
+        public TenantController(ILoggerManager logger, IRepositoryWrapper repository)
+        {
+            _logger = logger;
+            _repository = repository; 
+        }
+
+        [HttpGet]
+        public IActionResult GetAllTenants()
+        {
+            try
+            {
+                var tenants = _repository.Tenant.GetAlltenants(); 
+                _logger.LogInfo($"Returned al tenants from database.");
+                return Ok(tenants); 
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetAlltenants action : {ex.Message}"); 
+                return StatusCode(500, "Internal server error"); 
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTenantById(Guid id)
+        {
+            try
+            {
+                var tenant = _repository.Tenant.GetTenantById(id);
+
+                if (tenant == null)
+                {
+                    _logger.LogError($"Tenant with id: {id}, hasn't been found in db.");
+                    return NotFound(); 
+                }
+                else 
+                {
+                    _logger.LogInfo($"Returned tenant with id: {id}"); 
+                    return Ok(tenant);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetTenantById action: {ex.Message}"); 
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+
+        
+
+
+    }
+}
+
+
